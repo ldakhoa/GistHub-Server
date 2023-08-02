48af86eb0431ea37d1196b6ef683964699af60f8
Khoa Le <ldakhoa1308@gmail.com>
2023-08-02 19:55:35 +0700

    Add discover API

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 import { Hono } from "hono";
 import { ParserController } from "./Parser";
+import { Gist } from "./Gist";
 
 const app = new Hono();
+const host = "https://gist.github.com";
 
-app.get("/users/:username/starred", async (res) => {
-  const username = res.req.param("username");
-  const page = res.req.query("page");
+function createRoute(url: string) {
+  return async (res: {
+    req: { query: (arg: string) => any };
+    json: (arg: Gist[]) => any;
+  }) => {
+    const page = res.req.query("page");
+    const urlString = `${host}/${url}`;
+    const cache = caches.default;
+    const parser = new ParserController(urlString, cache);
 
-  const urlString = `https://gist.github.com/${username}/starred`;
-  const cache = caches.default;
-  const parser = new ParserController(urlString, cache);
+    const pageIndex = parseInt(page ?? "1");
+    const gists = await parser.parse(pageIndex);
 
-  const pageIndex = parseInt(page ?? "1");
-  const gists = await parser.parse(pageIndex);
+    return res.json(gists);
+  };
+}
 
-  return res.json(gists);
-});
+app.get("/users/:username/starred", createRoute(":username/starred"));
+app.get("/discover", createRoute("discover"));
+app.get("/discover/starred", createRoute("starred"));
+app.get("/discover/forked", createRoute("forked"));
 
 export default app;
