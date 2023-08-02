76e40463863462f329d227bee07c20f74cdc843c
Khoa Le <ldakhoa1308@gmail.com>
2023-08-03 00:59:49 +0700

    Fix get starred API

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Gist } from "./Gist";
 const app = new Hono();
 const host = "https://gist.github.com";
 
-function createRoute(url: string) {
+function createDiscoverRoute(url: string) {
   return async (res: {
     req: { query: (arg: string) => any };
     json: (arg: Gist[]) => any;
@@ -22,9 +22,22 @@ function createRoute(url: string) {
   };
 }
 
-app.get("/users/:username/starred", createRoute(":username/starred"));
-app.get("/discover", createRoute("discover"));
-app.get("/discover/starred", createRoute("starred"));
-app.get("/discover/forked", createRoute("forked"));
+app.get("/discover", createDiscoverRoute("discover"));
+app.get("/discover/starred", createDiscoverRoute("starred"));
+app.get("/discover/forked", createDiscoverRoute("forked"));
+
+app.get("/users/:username/starred", async (res) => {
+  const username = res.req.param("username");
+  const page = res.req.query("page");
+
+  const urlString = `https://gist.github.com/${username}/starred`;
+  const cache = caches.default;
+  const parser = new ParserController(urlString, cache);
+
+  const pageIndex = parseInt(page ?? "1");
+  const gists = await parser.parse(pageIndex);
+
+  return res.json(gists);
+});
 
 export default app;
