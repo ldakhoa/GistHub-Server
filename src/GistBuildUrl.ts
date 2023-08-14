c9e77216192a7198fcd90f3d69f1dcb68029c961
Khoa Le <ldakhoa1308@gmail.com>
2023-08-15 02:02:58 +0700

    Remove comment

--- src/GistBuildUrl.ts
+++ src/GistBuildUrl.ts
@@ -8,10 +8,6 @@ export function buildUrl(
     .filter(([_, value]) => value !== undefined)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
-  // .map(
-  //   ([key, value]) =>
-  //     `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`
-  // )
 
   const url = new URL(`${host}/${path}`);
   url.search = queryString;
